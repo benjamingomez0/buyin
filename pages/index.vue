ff4f24c7cd2d56065293cdde24badc9affe2b4e6
benjamingomez0 <benjamingomez0@gmail.com>
2021-09-27 16:39:38 -0700

    initial commit create cli project

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+  <Tutorial/>
+  <img src = "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" width="500" height="600" >
+
+</div>
+</template>
+
+<script>
+export default {}
+</script>
